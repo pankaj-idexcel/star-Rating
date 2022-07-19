96a3622d8e479c82a0b21b2212e70a0b71eef875
pankaj-idexcel <pankaj.b@idexcel.com>
2022-07-19 17:10:19 +0530

    made changes regarding svg files

--- src/Component/StarRating.jsx/index.jsx
+++ src/Component/StarRating.jsx/index.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import {ReactComponent as Star } from '../../Assets/star.svg'
-import {ReactComponent as StarFill } from '../../Assets/star.svg'
+import {ReactComponent as StarFill } from '../../Assets/star-fill.svg'
 
 const StarRating = ({ numberOfStars }) => {
+  
+  const [stars, setStars] = useState(0);
+
   const arr = [];
   for (let i = 1; i <= numberOfStars; i++) {
     arr.push(i);
   }
-  // console.log(arr)
-  const [stars, setStars] = useState(0);
+
+ 
   console.log("stars", stars);
 
   const handleStar = (star) => {
@@ -17,7 +20,8 @@ const StarRating = ({ numberOfStars }) => {
 
   console.log("updated-stars", stars);
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{display: "flex", flexDirection: "column" }}>
+    <div style={{ display: "flex", background: "grey", justifyContent: "center"}}>
       {arr.map((x) => (
         <div key={x} style={{ margin: "5px" }}>
         
@@ -31,6 +35,8 @@ const StarRating = ({ numberOfStars }) => {
           <div >{x}</div>
         </div>
       ))}
+    </div>
+    <div>You have given {stars} stars </div>
     </div>
   );
 };
